e454325bcbf141b1f42dc5a716b36c4f265f2a0e
mohaiminul69 <mohaiminul69@gmail.com>
2022-08-04 07:08:35 +0600

    add and delete from cart functions added

--- src/Redux/Slices/cartSlice.js
+++ src/Redux/Slices/cartSlice.js
@@ -6,8 +6,22 @@ export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart: (state, action) => {},
-    deleteFromCart: (state, action) => {},
+    addToCart: (state, action) => {
+      const { payload } = action;
+      const { cartProduct } = payload;
+      const isItemInCart = state.find((item) => item.id === cartProduct.id);
+      if (isItemInCart) {
+        return state.map((item) => item.id === cartProduct.id)
+          ? { ...item, ...cartProduct }
+          : item;
+      } else {
+        return [...state, { ...cartProduct }];
+      }
+    },
+    deleteFromCart: (state, action) => {
+      const { payload } = action;
+      return state.filter((item) => item.id !== payload.id);
+    },
     reset: () => initialState,
   },
 });
